aebc8881c186e19fffd19efe0eec9240fc8bc286
hk14004 <hardijs.kirsis@gmail.com>
2021-03-05 00:01:45 +0200

    Make Giphy service calls nicer

--- ChiGiphy/Services/Giphy/GiphyService.swift
+++ ChiGiphy/Services/Giphy/GiphyService.swift
@@ -19,28 +19,23 @@ class GiphyService: GiphyServiceProtocol {
     
     private let apiKey = "X09nb1IgWr4u7dxoMJbZ8iQs3ClrlAM0"
     
+    private let host = "http://api.giphy.com"
+    
     // MARK: Methods
     
     func search(text: String, offset: Int, limit: Int) -> Observable<[GiphyItem]> {
-        let url = URL(string: "http://api.giphy.com/v1/gifs/search")!
-        var request = URLRequest(url: url)
-        let keyQueryItem = URLQueryItem(name: "api_key", value: apiKey)
-        let searchQueryItem = URLQueryItem(name: "q", value: text)
-        let offsetQueryItem = URLQueryItem(name: "offset", value: "\(offset)")
-        let limitQueryItem = URLQueryItem(name: "limit", value: "\(limit)")
-        let urlComponents = NSURLComponents(url: url, resolvingAgainstBaseURL: true)!
+        guard var urlComp = URLComponents(string: host) else { return .just([]) }
+        urlComp.path = "/v1/gifs/search"
+        urlComp.queryItems = [
+            URLQueryItem(name: "api_key", value: apiKey),
+            URLQueryItem(name: "q", value: text),
+            URLQueryItem(name: "offset", value: "\(offset)"),
+            URLQueryItem(name: "limit", value: "\(limit)")
+        ]
         
-        request.httpMethod = "GET"
-        
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        urlComponents.queryItems = [searchQueryItem, keyQueryItem, offsetQueryItem, limitQueryItem]
-        
-        request.url = urlComponents.url!
-        
-        print(request.url!)
+        guard let requestUrl = urlComp.url else { return .just([]) }
         return URLSession.shared.rx
-            .decodable(request: request, type: GiphySearchResponse.self)
+            .decodable(request: URLRequest(url: requestUrl), type: GiphySearchResponse.self)
             .map {$0.data.elements}
     }
     
